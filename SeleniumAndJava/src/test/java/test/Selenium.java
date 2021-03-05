b93c044050310221e586476ac66954c123738d0d
garg-nikhil <nikhilgarg855@gmail.com>
2021-03-05 23:38:13 +0530

    DAY2

--- SeleniumAndJava/src/test/java/test/Selenium.java
+++ SeleniumAndJava/src/test/java/test/Selenium.java
@@ -2,19 +2,30 @@ package test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-
-
 public class Selenium {
-	
+
+	private WebDriver driver;
+
+	@BeforeTest
+	public void beforeTest() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Project Selenium\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
 	@Test
 	public void test() {
-	
-	System.setProperty("webdriver.chrome.driver", "C:\\Project Selenium\\chromedriver.exe");
-	WebDriver driver = new ChromeDriver();
-	
-	String URL = "https://www.amazon.in";
-	driver.get(URL);
-	driver.close();
-}}
+
+		String URL = "https://www.amazon.in";
+		driver.manage().window().maximize();
+		driver.get(URL);
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+}
